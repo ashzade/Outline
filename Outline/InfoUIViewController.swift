5a3094c7eda7fbd2358064c44944752e360108b5
Ash Ahmadzadeh <ash@lemonlimedesigns.ca>
2018-03-17 13:03:56 -0400

    Replaced info screen with webview to outlinenotes.com

---
+++ Outline/InfoUIViewController.swift
@@ -0,0 +1,23 @@
+//
+//  InfoUIViewController.swift
+//  Outline
+//
+//  Created by Ash Zade on 2018-03-17.
+//  Copyright © 2018 Ash Zade. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class InfoUIViewControler: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "http://www.outlinenotes.com");
+        let request = URLRequest(url: url!);
+        webView.load(request);
+    }
+}
